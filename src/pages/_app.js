44f3d42b98bf3cdafff53eb638b68b6f5757841f
kaysenchapman <kaysenchapman@gmail.com>
2025-12-12 23:16:19 +0000

    trying to deploy to vercel

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import { SessionProvider } from 'next-auth/react';
 import { BookProvider } from '../context/BookContext';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/Navbar.js';
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
